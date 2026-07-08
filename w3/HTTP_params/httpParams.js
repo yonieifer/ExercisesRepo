8d494f90b1b5929123e350b1947c02537cf4772a
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-08 11:26:52 +0300

    add HTTP params exe

--- w3/HTTP_params/httpParams.js
+++ w3/HTTP_params/httpParams.js
@@ -1,53 +1,63 @@
-// 1
-const extractId = (url) => {
-    const parts = url.split("/");
-    return parts[2];
-};
+// // 1
+// const extractId = (url) => {
+//     const parts = url.split("/");
+//     return parts[2];
+// };
 
-// 2
-const parseQuery = (url) => {
-    const query = url.split("?")[1] || "";
-    console.log(query);
+// // 2
+// const parseQuery = (url) => {
+//     const query = url.split("?")[1] || "";
+//     console.log(query);
 
-    const parts = query.split("&").map((detail) => detail.split("="));
-    console.log(parts);
+//     const parts = query.split("&").map((detail) => detail.split("="));
+//     console.log(parts);
 
-    const obj = Object.fromEntries(parts);
-    return obj;
-};
+//     const obj = Object.fromEntries(parts);
+//     return obj;
+// };
 
-// 3
-const getQueryParams = (rawUrl) => {
-    const parsed = new URL(rawUrl, "http://localhost");
-    const qs = parsed.searchParams;
-    return {
-        page: qs.get("page") || "1",
-        limit: qs.get("limit") || "10",
-        sort: qs.get("sort") || "default"
-    }
-};
+// // 3
+// const getQueryParams = (rawUrl) => {
+//     const parsed = new URL(rawUrl, "http://localhost");
+//     const qs = parsed.searchParams;
+//     return {
+//         page: qs.get("page") || "1",
+//         limit: qs.get("limit") || "10",
+//         sort: qs.get("sort") || "default"
+//     }
+// };
 
-// console.log(getQueryParams("/users?page=3&sort=name"));
+// // console.log(getQueryParams("/users?page=3&sort=name"));
 
-// 4
-const users = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-];
+// // 4
+// const users = [
+//   { id: 1, name: "Alice" },
+//   { id: 2, name: "Bob" },
+// ];
 
-const getParam = (url, pattern) => {
-    const pp = pattern.split("/")
-    const up = url.split("/")
+// const getParam = (url, pattern) => {
+//     const pp = pattern.split("/")
+//     const up = url.split("/")
 
-    const key = pp.find(s => s.startsWith(":"))?.slice(1)
-    const index = pp.findIndex(s => s.startsWith(":"))
+//     const key = pp.find(s => s.startsWith(":"))?.slice(1)
+//     const index = pp.findIndex(s => s.startsWith(":"))
 
-    return {
-        [key]: up[index]
-    }
-}
+//     return {
+//         [key]: up[index]
+//     }
+// }
 
-const { id } = getParam(url, "/users/:id");
-const user   = users.find(u => u.id === +id);
+// const { id } = getParam(url, "/users/:id");
+// const user   = users.find(u => u.id === +id);
 
 // 5
+const handler = (req, res) => {
+    const reqUrl = new URL(req.url, "http://localhost")
+    const queries = reqUrl.searchParams
+    const role = queries.get("role")
+    const page = queries.get("page")
+    const limit = queries.get("limit")
+
+    let result = "users"
+
+}
